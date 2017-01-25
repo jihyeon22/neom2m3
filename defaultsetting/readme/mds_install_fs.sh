d2174f45d1ed5e3726cce214032e4ee97470db13
kksworks <kksworks@gmail.com>
2017-01-25 09:07:47 +0900

    fix bug

--- defaultsetting/readme/mds_install_fs.sh
+++ defaultsetting/readme/mds_install_fs.sh
@@ -4,19 +4,19 @@ cp -rf /system/mds/install_files/readme/files/mds_conf.sh /etc/init.d/
 cp -rf /system/mds/install_files/readme/files/mds_init.sh /etc/init.d/
 
 cd /etc/rc1.d/ 
-ln -s ../init.d/mds_init.sh S99mds_init
+ln -s ../init.d/mds_init.sh S99mds_init.sh
 
 cd /etc/rc2.d/ 
-ln -s ../init.d/mds_init.sh S99mds_init
+ln -s ../init.d/mds_init.sh S99mds_init.sh
 
 cd /etc/rc3.d/ 
-ln -s ../init.d/mds_init.sh S99mds_init
+ln -s ../init.d/mds_init.sh S99mds_init.sh
 
 cd /etc/rc4.d/ 
-ln -s ../init.d/mds_init.sh S99mds_init
+ln -s ../init.d/mds_init.sh S99mds_init.sh
 
 cd /etc/rc5.d/ 
-ln -s ../init.d/mds_init.sh S99mds_init
+ln -s ../init.d/mds_init.sh S99mds_init.sh
 
 
 
