3dbc240f48d9763cecef8b2529c2956ae070397b
kksworks <kksworks@gmail.com>
2017-01-25 09:05:24 +0900

    install script

---
+++ defaultsetting/readme/mds_install_fs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cp -rf /system/mds/install_files/readme/files/mds_conf.sh /etc/init.d/
+cp -rf /system/mds/install_files/readme/files/mds_init.sh /etc/init.d/
+
+cd /etc/rc1.d/ 
+ln -s ../init.d/mds_init.sh S99mds_init
+
+cd /etc/rc2.d/ 
+ln -s ../init.d/mds_init.sh S99mds_init
+
+cd /etc/rc3.d/ 
+ln -s ../init.d/mds_init.sh S99mds_init
+
+cd /etc/rc4.d/ 
+ln -s ../init.d/mds_init.sh S99mds_init
+
+cd /etc/rc5.d/ 
+ln -s ../init.d/mds_init.sh S99mds_init
+
+
+
